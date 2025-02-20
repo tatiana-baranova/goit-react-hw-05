2342ed1459c6d4b9b20c53a117aa392c2a51511d
tetiana_baranova <syniovych.tanya@gmail.com>
2025-02-20 23:17:16 +0200

    url api

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTkxYzZmOWY4MDNkNTRjZjFjMGY0OTNjZmI0YTFmNSIsIm5iZiI6MTc0MDA3MjM4My4wODYwMDAyLCJzdWIiOiI2N2I3NjViZjQ0NGRkN2ZjZWZiYTExMGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.yqPXzI2hDNp6xqFhzyaLfizzk6_WZ_ZG6G2pyIC6Ltw';
 
 export const searchMovieTrending = async () => {
-    const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+    const url = `https://api.themoviedb.org/3/trending/movie/day`;
+        
+        // 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
 
     const options = {
     headers: {
@@ -24,7 +26,9 @@ export const searchMovieTrending = async () => {
 };
 
 export const fetchMovieSearch = async (query, page = 1) => {
-    const url = `https://api.themoviedb.org/3/search/movie?query=${query}include_adult=false&language=en-US&page=${page}`;
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&page=${page}`;
+    
+        // `https://api.themoviedb.org/3/search/movie?query=${query}include_adult=false&language=en-US&page=${page}`;
 
     const options = {
         headers: {
@@ -44,7 +48,9 @@ export const fetchMovieSearch = async (query, page = 1) => {
 };
 
 export const fetchMovieDetails = async (movieId) => {
-    const url = `https://api.themoviedb.org/3/movie/movie_id=${movieId}?language=en-US`;
+    const url = `https://api.themoviedb.org/3/movie/movie_id=${movieId}`;
+
+        // `https://api.themoviedb.org/3/movie/movie_id=${movieId}?language=en-US`;
 
     const options = {
         headers: {
@@ -64,7 +70,9 @@ export const fetchMovieDetails = async (movieId) => {
 }
 
 export const fetchMovieCredits = async (movieId) => {
-    const url = `https://api.themoviedb.org/3/movie/movie_id=${movieId}/credits?language=en-US`;
+    const url = `https://api.themoviedb.org/3/movie/movie_id=${movieId}/credits`;
+        
+        // `https://api.themoviedb.org/3/movie/movie_id=${movieId}/credits?language=en-US`;
 
     const options = {
         headers: {
@@ -85,7 +93,9 @@ export const fetchMovieCredits = async (movieId) => {
 
 
 export const fetchMovieReviews = async (movieId) => {
-    const url = `https://api.themoviedb.org/3/movie/movie_id=${movieId}/reviews?language=en-US&page=1`;
+    const url = `https://api.themoviedb.org/3/movie/movie_id=${movieId}/reviews`;
+        
+        // `https://api.themoviedb.org/3/movie/movie_id=${movieId}/reviews?language=en-US&page=1`;
 
     const options = {
         headers: {
